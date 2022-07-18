9ea9b55f269fd2aa01e4e2eefdda4ba070f8775b
NocturnallDev <m.nocturnal@outlook.com>
2022-07-18 11:12:58 +0530

    s

--- app/__init__.py
+++ app/__init__.py
@@ -23,13 +23,13 @@ def create_app() -> Quart:
 
 
 def register_blueprints(app: Quart):
-    from app.app import admin
-    from app.app import auth
-    from app.app import core
+    from app.admin import admin_blueprint
+    from app.admin import auth_blueprint
+    from app.core import core_blueprint
     
-    app.register_blueprint(admin.admin_blueprint)
-    app.register_blueprint(auth.auth_blueprint)
-    app.register_blueprint(core.core_blueprint)
+    app.register_blueprint(admin_blueprint)
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(core_blueprint)
 
 
 def initialize_database(app: Quart):
