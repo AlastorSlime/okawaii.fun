41ac92812a191685bb12ec7ba8d57fa9ebed3518
NetFlake <109495122+NetflakeDev@users.noreply.github.com>
2022-07-26 19:54:34 -0700

    Quick fix

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ if __name__ == '__main__':
     app = create_app()
     uvicorn.run(
         app,
-        # host="127.0.0.0", # For production.
-        port=5000
+        host="127.0.0.0", # For production.
     )
